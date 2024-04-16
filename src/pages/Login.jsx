ddde166a9da1876d5a39ecb6a91c9f25446db8f7
Aadarsh Jha <aadarshjha1401@gmail.com>
2024-04-16 11:15:54 +0530

    new feature implemented v2

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,3 +1,21 @@
-hi =55 -=787
-54
-1aafasfaweqtfasf4q3qerwfdsvacxzzvsadfa
+import React, { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import { VscEye } from "react-icons/vsc"
+import { VscEyeClosed } from "react-icons/vsc"
+
+const Login = () => {
+  const navigate = useNavigate()
+
+  const [email, setEmail] = useState("")
+  const [pwd, setPwd] = useState("")
+  const [showPwd, setShowPwd] = useState(false)
+
+  const handleLogin = () => {
+    console.log(email, pwd)
+    navigate("/")
+  }
+
+  return <></>
+}
+
+export default Login
